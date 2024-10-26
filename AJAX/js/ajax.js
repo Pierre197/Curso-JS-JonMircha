4aee7b39716ca96f3752bd3a94c92e249fd7046d
Pierre Castillo <pierre.cll.19.04@gmail.com>
2024-10-26 12:37:16 -0500

    Clase 107 Ajax Fetch - JS

--- AJAX/js/ajax.js
+++ AJAX/js/ajax.js
@@ -30,3 +30,32 @@
      xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
      xhr.send();
 })();
+
+(() =>{
+    const $fetch = document.getElementById("fetch"),
+     $fragment = document.createDocumentFragment();
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+    /* .then(res => {
+        console.log(res);
+        return res.ok ? res.json(): Promise.reject();
+    }) */
+    .then((res)=> res.ok ? res.json():Promise.reject(res))
+    .then(json => {
+        console.log(json);
+        json.forEach(el => {
+            const $li = document.createElement("li");
+            $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+            $fragment.appendChild($li);
+        });
+        fetch.appendChild($fragment);
+    })
+    .catch(err => {
+        console.log(err);
+        let message = err.statusText || "Ocurrió un error";
+        $fetch.innerHTML = `"Error ${err.status}: ${message}`;
+    }).finally(() => {
+        console.log("Esto se ejecutara independientemente del resultado de la promesa fetch");
+    });
+
+})();
