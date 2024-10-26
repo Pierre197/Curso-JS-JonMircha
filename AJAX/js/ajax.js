c5438df050dba6a78975181e9f4509c13bd2ffbb
Pierre Castillo <pierre.cll.19.04@gmail.com>
2024-10-26 16:11:15 -0500

    Clase 109 API Fetch Asyn - JS

--- AJAX/js/ajax.js
+++ AJAX/js/ajax.js
@@ -1,9 +1,11 @@
+/*XMLHttpRequest */
+
 (() => {
     const xhr = new XMLHttpRequest(),
      $xhr = document.getElementById("xhr"),
      $fragment = document.createDocumentFragment();
 
-     console.log(xhr);
+    //  console.log(xhr);
 
      xhr.addEventListener("readystatechange", e => {
         if (xhr.readyState !== 4) return;
@@ -24,13 +26,13 @@
             $xhr.innerHTML = `"Error ${xhr.status}: ${message}`;
         }
 
-        console.log("Este mensaje cargara de cualquier forma");
+        // console.log("Este mensaje cargara de cualquier forma");
      });
 
      xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
      xhr.send();
 })();
-
+/*Fetch */
 (() =>{
     const $fetch = document.getElementById("fetch"),
      $fragment = document.createDocumentFragment();
@@ -40,22 +42,58 @@
         console.log(res);
         return res.ok ? res.json(): Promise.reject();
     }) */
-    .then((res)=> res.ok ? res.json():Promise.reject(res))
+    .then((res)=> res.ok ? res.json(): Promise.reject(res))
     .then(json => {
-        console.log(json);
+        // console.log(json);
         json.forEach(el => {
             const $li = document.createElement("li");
             $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
             $fragment.appendChild($li);
         });
-        fetch.appendChild($fragment);
+        $fetch.appendChild($fragment);
     })
     .catch(err => {
-        console.log(err);
+        // console.log(err);
         let message = err.statusText || "Ocurrió un error";
-        $fetch.innerHTML = `"Error ${err.status}: ${message}`;
+        $fetch.innerHTML = `Error ${err.status}: ${message}`;
     }).finally(() => {
-        console.log("Esto se ejecutara independientemente del resultado de la promesa fetch");
+        // console.log("Esto se ejecutara independientemente del resultado de la promesa fetch");
     });
 
+})();
+
+/* Api Fetch */
+
+(()=>{
+    const $fetchAsync = document.getElementById("fetch-async"),
+    $fragment = document.createDocumentFragment();
+
+    async function getData(){
+        try {
+            let res = await fetch("https://jsonplaceholder.typicode.com/users"),
+             json = await res.json();
+
+            // if (!res.ok) throw
+            if (!res.ok) throw {status: res.status, statusText: res.statusText}
+            
+            json.forEach(el => {
+                const $li = document.createElement("li");
+                $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+                $fragment.appendChild($li);
+            });
+
+            $fetchAsync.appendChild($fragment);
+
+            console.log(res, json);
+        } catch (err) {
+            //console.log("Estoy en el catch", err);
+            let message = err.statusText || "Ocurrió un error";
+            $fetchAsync.innerHTML = `"Error ${err.status}: ${message}`;
+        } finally {
+            console.log("Esto se ejecutara independientemente del try catch");
+        }
+        
+    }
+
+    getData();
 })();
